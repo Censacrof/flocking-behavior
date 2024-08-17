cc486b0e8fa55d8c4667847b9e2486da01eccc78
Francesco Galisi <galisifrancesco@gmail.com>
2024-08-17 15:06:45 +0200

    Cube example

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,57 @@
+import * as THREE from "three";
+import { useEffect, useRef } from "react";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <ThreeContaier />
+    </>
+  );
+}
+
+function ThreeContaier() {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const target = ref.current;
+    if (!target) {
+      return;
+    }
+
+    setupScene(target);
+
+    return () => {
+      target.innerHTML = "";
+    };
+  }, []);
+
+  return <div ref={ref} />;
+}
+
+function setupScene(targetDiv: HTMLDivElement) {
+  const WIDTH = 600;
+  const HEIGHT = 400;
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(WIDTH, HEIGHT);
+  targetDiv.appendChild(renderer.domElement);
+
+  function animate() {
+    renderer.render(scene, camera);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
+  renderer.setAnimationLoop(animate);
 }
