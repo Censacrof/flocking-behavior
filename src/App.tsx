15b136acff8158fc037685a4c1c961b983167b8f
Francesco Galisi <galisifrancesco@gmail.com>
2024-08-17 16:10:43 +0200

    Add sky

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Sky } from "three/addons/objects/Sky.js";
 import * as THREE from "three";
 import { useEffect, useRef } from "react";
 
@@ -36,6 +37,9 @@ function setupScene(targetDiv: HTMLDivElement) {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
 
+  const gridHelper = new THREE.GridHelper();
+  scene.add(gridHelper);
+
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshPhysicalMaterial({
     color: 0x00ff00,
@@ -43,23 +47,48 @@ function setupScene(targetDiv: HTMLDivElement) {
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
-  scene.add(ambientLight);
+  // const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
+  // scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  scene.add(directionalLight);
+  // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  // scene.add(directionalLight);
+  // scene.add(directionalLight.target);
 
-  camera.position.z = 5;
+  // const directionalLightHelper = new THREE.DirectionalLightHelper(
+  //   directionalLight,
+  //   1,
+  // );
+  // scene.add(directionalLightHelper);
+
+  const CAMERA_RADIUS = 5;
+  const CAMERA_ANGULAR_SPEED = 0.01;
+  camera.position.y = 1;
+
+  const sky = new Sky();
+  sky.scale.setScalar(450000);
+
+  const phi = THREE.MathUtils.degToRad(90);
+  const theta = THREE.MathUtils.degToRad(180);
+  const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
+
+  sky.material.uniforms.sunPosition.value = sunPosition;
+  scene.add(sky);
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(WIDTH, HEIGHT);
   targetDiv.appendChild(renderer.domElement);
 
+  let cameraAngle = 0;
   function animate() {
-    renderer.render(scene, camera);
-
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    cameraAngle += CAMERA_ANGULAR_SPEED;
+    camera.position.x = CAMERA_RADIUS * Math.cos(cameraAngle);
+    camera.position.z = CAMERA_RADIUS * Math.sin(cameraAngle);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    renderer.render(scene, camera);
   }
   renderer.setAnimationLoop(animate);
 }
