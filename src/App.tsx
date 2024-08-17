7cab8eb1f1ad5df27feb075d0988a8ee95b62c62
Francesco Galisi <galisifrancesco@gmail.com>
2024-08-17 17:04:21 +0200

    Lighting based on sky

--- src/App.tsx
+++ src/App.tsx
@@ -47,32 +47,50 @@ function setupScene(targetDiv: HTMLDivElement) {
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
-  // const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
-  // scene.add(ambientLight);
-
-  // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  // scene.add(directionalLight);
-  // scene.add(directionalLight.target);
-
-  // const directionalLightHelper = new THREE.DirectionalLightHelper(
-  //   directionalLight,
-  //   1,
-  // );
-  // scene.add(directionalLightHelper);
-
-  const CAMERA_RADIUS = 5;
-  const CAMERA_ANGULAR_SPEED = 0.01;
-  camera.position.y = 1;
-
   const sky = new Sky();
   sky.scale.setScalar(450000);
 
   const phi = THREE.MathUtils.degToRad(90);
   const theta = THREE.MathUtils.degToRad(180);
   const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
-
   sky.material.uniforms.sunPosition.value = sunPosition;
+
   scene.add(sky);
+
+  const MAX_AMBIENT_LIGHT = 0.5;
+  const MIN_AMBIENT_LIGHT = 0.05;
+  const sunContribution = Math.max(
+    0,
+    new THREE.Vector3(0, 1, 0).dot(sunPosition.normalize()),
+  );
+  const hemisphereLightIntensity = Math.max(
+    MAX_AMBIENT_LIGHT * sunContribution,
+    MIN_AMBIENT_LIGHT,
+  );
+  const hemisphereLight = new THREE.HemisphereLight(
+    0xffffff,
+    0xffffff,
+    hemisphereLightIntensity,
+  );
+  scene.add(hemisphereLight);
+  scene.add(new THREE.HemisphereLightHelper(hemisphereLight, 20));
+
+  const MAX_DIRECTIONAL_LIGHT = 0.8;
+  const directionalLightIntensity = MAX_DIRECTIONAL_LIGHT;
+  const directionalLight = new THREE.DirectionalLight(
+    0xffffff,
+    directionalLightIntensity,
+  );
+  scene.add(directionalLight);
+  scene.add(directionalLight.target);
+  directionalLight.position.set(sunPosition.x, sunPosition.y, sunPosition.z);
+  directionalLight.target.position.set(0, 0, 0);
+
+  scene.add(new THREE.DirectionalLightHelper(directionalLight, 1));
+
+  const CAMERA_RADIUS = 5;
+  const CAMERA_ANGULAR_SPEED = 0.01;
+  camera.position.y = 1;
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(WIDTH, HEIGHT);
@@ -80,13 +98,13 @@ function setupScene(targetDiv: HTMLDivElement) {
 
   let cameraAngle = 0;
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     cameraAngle += CAMERA_ANGULAR_SPEED;
     camera.position.x = CAMERA_RADIUS * Math.cos(cameraAngle);
     camera.position.z = CAMERA_RADIUS * Math.sin(cameraAngle);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
   }
