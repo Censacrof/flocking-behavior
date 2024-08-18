c09bf7fd5fc5db7b380208a7ab15e58c5129af83
Francesco Galisi <galisifrancesco@gmail.com>
2024-08-18 14:53:59 +0200

    Implement separation and alignment rules7

--- src/scene/Fock.ts
+++ src/scene/Fock.ts
@@ -1,15 +1,24 @@
-import { Object3D } from "three";
+import { Object3D, Vector3 } from "three";
 import { Bird } from "./Bird";
 import { Entity } from "./Entity";
+import { VerletObject3D } from "./VerletObject3D";
 
 export class Flock extends Object3D implements Entity {
-  BIRDS_NUMBER = 20;
+  BIRDS_NUMBER = 40;
+
+  SEPARATION_RADIUS = 0.5;
+  SEPARATION_FORCE = 0;
+
+  ALIGNMENT_RADIUS = 2;
+  ALIGNMENT_FORCE = 4;
+
+  COHESION_RADIUS = 3;
 
   birds;
   constructor() {
     super();
 
-    this.birds = [...Array(20)].map(() => {
+    this.birds = [...Array(this.BIRDS_NUMBER)].map(() => {
       const bird = new Bird();
       this.add(bird);
 
@@ -18,6 +27,58 @@ export class Flock extends Object3D implements Entity {
   }
 
   update(delta: number): void {
+    this.applyFlockingBehaviour(this.birds);
+
     this.birds.forEach((bird) => bird.update(delta));
+  }
+
+  applyFlockingBehaviour(birds: VerletObject3D[]) {
+    birds.forEach((currentBird, currentIndex) => {
+      const birdsInAlignmentRange: VerletObject3D[] = [];
+      const birdsInCohesionRange: VerletObject3D[] = [];
+
+      birds.forEach((targetBird, targetIndex) => {
+        if (currentIndex === targetIndex) {
+          return;
+        }
+
+        const currentPos = currentBird.position.clone();
+        const targetPos = targetBird.position.clone();
+        const diff = targetPos.clone().sub(currentPos.clone());
+        const distance = diff.length();
+
+        // apply separation right awayt
+        if (distance < this.SEPARATION_RADIUS) {
+          currentBird.applyForce(
+            diff.clone().normalize().multiplyScalar(-this.SEPARATION_FORCE),
+          );
+        }
+
+        if (distance < this.ALIGNMENT_RADIUS) {
+          birdsInAlignmentRange.push(targetBird);
+        }
+
+        if (distance < this.COHESION_RADIUS) {
+          birdsInCohesionRange.push(targetBird);
+        }
+      });
+
+      // apply alignment
+      if (birdsInAlignmentRange.length > 0) {
+        const avgAlignment = new Vector3(0, 0, 0);
+        birdsInAlignmentRange.forEach((b) =>
+          avgAlignment.add(b.velocity.clone()),
+        );
+        avgAlignment.divideScalar(birdsInAlignmentRange.length);
+
+        const speedDiff = avgAlignment
+          .clone()
+          .sub(currentBird.position.clone());
+
+        currentBird.applyForce(
+          speedDiff.clone().clampLength(0, this.ALIGNMENT_FORCE),
+        );
+      }
+    });
   }
 }
