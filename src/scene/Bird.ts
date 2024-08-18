c4472b3dd02e9e3e76e39920fa632ab0739e2139
Francesco Galisi <galisifrancesco@gmail.com>
2024-08-18 02:20:24 +0200

    Add bounding sphere

--- src/scene/Bird.ts
+++ src/scene/Bird.ts
@@ -1,9 +1,10 @@
-import { ConeGeometry, Mesh, MeshPhysicalMaterial } from "three";
+import { ConeGeometry, Mesh, MeshPhysicalMaterial, Vector3 } from "three";
 import { VerletObject3D } from "./VerletObject3D";
 import { degToRad } from "three/src/math/MathUtils.js";
 
 export class Bird extends VerletObject3D {
-  speed = 1;
+  TARGET_SPEED = 3;
+  MAX_DISTANCE_FROM_ORIGIN = 5;
 
   constructor() {
     super();
@@ -18,21 +19,33 @@ export class Bird extends VerletObject3D {
     mesh.position.setZ(-coneHeight / 2);
     mesh.rotateX(degToRad(-90));
     this.add(mesh);
+
+    this.position.random().multiplyScalar(this.MAX_DISTANCE_FROM_ORIGIN);
+    this.velocity.randomDirection().multiplyScalar(this.TARGET_SPEED);
   }
 
-  framesLeft = 0;
   public update(delta: number) {
-    this.framesLeft -= 1;
-    if (this.framesLeft <= 0) {
-      this.velocity.randomDirection().multiplyScalar(this.speed);
-      this.framesLeft = 120;
-    }
-
     this.lookAt(
       this.position
         .clone()
         .add(this.velocity.clone().multiplyScalar(-1).normalize()),
     );
+
+    if (this.position.clone().length() > this.MAX_DISTANCE_FROM_ORIGIN) {
+      this.position.normalize().multiplyScalar(this.MAX_DISTANCE_FROM_ORIGIN);
+      this.applyForce(this.position.clone().normalize().multiplyScalar(-1));
+    }
+
+    if (this.velocity.clone().length() > this.TARGET_SPEED) {
+      this.applyForce(
+        this.velocity
+          .clone()
+          .normalize()
+          .multiplyScalar(this.TARGET_SPEED)
+          .sub(this.velocity.clone())
+          .multiplyScalar(0.5),
+      );
+    }
 
     super.update(delta);
   }
